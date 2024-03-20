bf2fb026c6019fcc5ed07d1051c504bf8cda447d
renata <renata2095@mail.ru>
2024-03-20 21:53:19 +0300

    Разделение функций создания комментариев и рендера. Не работает показ комментариев

--- js/full-photo.js
+++ js/full-photo.js
@@ -2,32 +2,38 @@ import { isEscapeKey } from './utils';
 
 const body = document.querySelector('body');
 const bigPicture = document.querySelector('.big-picture');
-const bigPictureImage = document.querySelector('.big-picture__img img');
-const pictureCloseButton = document.querySelector('.big-picture__cancel');
+const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
+const pictureCloseButton = bigPicture.querySelector('.big-picture__cancel');
 const likesCount = bigPicture.querySelector('.likes-count');
 const pictureDescription = bigPicture.querySelector('.social__caption');
 const socialComments = bigPicture.querySelector('.social__comments');
 
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 
-const commentCount = bigPicture.querySelector('.social__comment-count');
-const commentShowCount = bigPicture.querySelector('.social__comment-shown-count');
+// const commentCount = bigPicture.querySelector('.social__comment-count');
+// const commentShowCount = bigPicture.querySelector('.social__comment-shown-count');
 const commentTotalCount = document.querySelector('.social__comment-total-count');
-const commentsLoader = bigPicture.querySelector('.comments-loader');
+// const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+// Создание комментария к фотографии
+const createComments = ({avatar, name, message}) => {
+  const commentsElement = commentTemplate.cloneNode(true);
+
+  const peoplesAvatar = bigPicture.querySelector('.social__picture');
+  const peoplesMessage = bigPicture.querySelector('.social__text');
+  peoplesAvatar.src = avatar;
+  peoplesAvatar.alt = name;
+  peoplesMessage.textContent = message;
+
+  return commentsElement;
+};
 
 // Рендер комментариев к фотографии
 const renderComments = (comments) => {
   const commentsListFragment = document.createDocumentFragment();
 
   comments.forEach((comment) => {
-    const commentElement = commentTemplate.cloneNode(true);
-
-    const peoplesAvatar = document.querySelector('.social__picture');
-    const peoplesMessage = document.querySelector('.social__text');
-    peoplesAvatar.src = comment.avatar;
-    peoplesAvatar.alt = comment.name;
-    peoplesMessage.textContent = comment.message;
-
+    const commentElement = createComments(comment);
     commentsListFragment.append(commentElement);
   });
 
@@ -41,12 +47,8 @@ export const openBigPicture = (dataPhoto) => {
   bigPictureImage.src = dataPhoto.url;
   likesCount.textContent = dataPhoto.likes;
   pictureDescription.innerHTML = dataPhoto.description;
-  commentShowCount.textContent = dataPhoto.comments;
   commentTotalCount.textContent = dataPhoto.comments.length;
   renderComments(dataPhoto.comments);
-
-  commentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
 };
